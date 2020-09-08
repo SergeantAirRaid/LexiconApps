aea02657d91415bd48b02abc13fa9ef2d1766eaf
SergeantAirRaid <cortriaj@gmail.com>
2020-09-07 17:35:07 -0700

    VMS refactor, seems to work

---
+++ Shared/ViewModelSupport/README.md
@@ -0,0 +1,4 @@
+# "ViewModelSupport" here is a modified, stripped down version of the code found here: 
+#	https://github.com/BrianGenisio/ViewModelSupport
+#
+# Examples on how to use this class can be found in the attached test app.
---
+++ Shared/ViewModelSupport/ViewModelSupportTestApp/ViewModels/MainWindowVM.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ViewModelSupport;
+
+namespace ViewModelSupportTestApp
+{
+    class MainWindowVM : ViewModelBase
+    {
+        public string TextBoxText { get; set; } = "Enter text...";
+        public string LabelText { get; set; } = "Label Text";
+
+        public ObservableCollection<string> History { get; set; } = new ObservableCollection<string>();
+
+        public void Exec_Enter()
+        {
+            LabelText = TextBoxText;
+            RaisePropertyChanged("LabelText"); // strings don't seem to automatically raise their changed event, so do it manually.
+            
+            History.Add(TextBoxText); // Observable collections DO seem to automatically raise their changed event.
+        }
+
+
+    }
+}
